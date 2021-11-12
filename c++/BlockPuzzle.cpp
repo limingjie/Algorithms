19947c0fbfb2a66f3cd727adcd53845cbed596db
mingjie.li <mingjie.li@everbridge.com>
2021-11-12 18:36:20 +0800

    BlockPuzzle - Optimization

--- c++/BlockPuzzle.cpp
+++ c++/BlockPuzzle.cpp
@@ -10,12 +10,10 @@
 #include <algorithm>  // std::sort
 #include <iomanip>    // std::setw
 #include <iostream>   // std::cout,   std::endl
+#include <map>        // Ordered Map
 #include <string>     // std::string, std::to_string
 #include <utility>    // std::pair,   std::make_pair
-#include <vector>
-
-typedef std::vector<std::pair<size_t, size_t>> BlockData;
-typedef std::vector<size_t>                    BoardData;
+#include <vector>     // std::vector
 
 // Debug Tracking
 static size_t count_setblock   = 0;
@@ -26,18 +24,21 @@ static size_t count_checkcell  = 0;
 class Block
 {
 public:
-    Block(const std::string &cells);
+    typedef std::vector<std::pair<size_t, size_t>> Data;
+    typedef std::vector<Block>                     Blocks;
+
+    Block(size_t index, const std::string &cells);
     ~Block();
 
-    void Print(const std::string &title = "Block") const;
-    void Print(size_t board_width, size_t board_height, size_t x, size_t y,
-               const std::string &title = "Block") const;
+    size_t GetIndex() const;
+    void   Print() const;
 
 private:
     friend class Board;
 
-    BlockData data;
+    Data data;
 
+    size_t index;
     size_t width;
     size_t height;
     double weight;
@@ -45,39 +46,41 @@ private:
     void SetCell(size_t x, size_t y);
 };
 
-typedef std::vector<Block> Blocks;
-
 class Board
 {
 public:
+    typedef std::vector<size_t>                         Data;
+    typedef std::map<size_t, std::pair<size_t, size_t>> Solution;
+
     Board(size_t width, size_t height);
     ~Board();
 
-    bool Resolve(const Blocks &blocks);
-    bool SetBlock(const Block &block, size_t x, size_t y);
-
-    void Print(const std::string &title = "Board") const;
+    Solution Solve(const Block::Blocks &blocks);
+    bool     SetBlock(const Block &block, size_t x, size_t y);
+    void     Print() const;
 
 private:
-    BoardData data;
+    Data     data;
+    Solution steps;
 
     size_t width;
     size_t height;
 
-    void SetCell(size_t x, size_t y);
+    void SetCell(size_t x, size_t y, size_t value = 0);
     bool CheckCell(size_t x, size_t y) const;
     void ClearCell(size_t x, size_t y);
     bool CheckBlock(const Block &block, size_t x, size_t y) const;
     void ClearBlock(const Block &block, size_t x, size_t y);
-    bool FindSolution(const Blocks &blocks);
+    bool Search(const Block::Blocks &blocks);
 };
 
-Block::Block(const std::string &cells)
+Block::Block(size_t index, const std::string &cells)
 {
-    data   = BlockData();
-    width  = 0;
-    height = 0;
-    weight = 0;
+    data        = Data();
+    this->index = index;
+    width       = 0;
+    height      = 0;
+    weight      = 0;
 
     size_t x = 0;
     size_t y = 0;
@@ -107,6 +110,8 @@ Block::~Block()
     // std::cout << "Block Released." << std::endl;
 }
 
+size_t Block::GetIndex() const { return index; }
+
 // Compose a block
 void Block::SetCell(size_t x, size_t y)
 {
@@ -116,14 +121,11 @@ void Block::SetCell(size_t x, size_t y)
     if (y >= height) height = y + 1;
 }
 
-void Block::Print(const std::string &title) const { Print(width, height, 0, 0, title); }
-
-void Block::Print(size_t board_width, size_t board_height, size_t block_x, size_t block_y,
-                  const std::string &title) const
+void Block::Print() const
 {
-    Board b(board_width, board_height);
-    b.SetBlock(*this, block_x, block_y);
-    b.Print(title);
+    Board b(width, height);
+    b.SetBlock(*this, 0, 0);
+    b.Print();
 }
 
 Board::Board(size_t width, size_t height)
@@ -131,7 +133,7 @@ Board::Board(size_t width, size_t height)
     this->width  = width;
     this->height = height;
 
-    data = BoardData(width * height, 0);
+    data = Data(width * height, -1);
 }
 
 Board::~Board()
@@ -140,21 +142,21 @@ Board::~Board()
 }
 
 // SetCell cell (x, y) = 1
-inline void Board::SetCell(size_t x, size_t y)
+inline void Board::SetCell(size_t x, size_t y, size_t value)
 {
     count_setcell++;
-    data[y * width + x] = 1;
+    data[y * width + x] = value;
 }
 
 // Check if cell (x, y) is empty
 bool Board::CheckCell(size_t x, size_t y) const
 {
     count_checkcell++;
-    return (data[y * width + x] == 0);
+    return (data[y * width + x] == -1);
 }
 
 // SetCell cell (x, y) = 0
-void Board::ClearCell(size_t x, size_t y) { data[y * width + x] = 0; }
+void Board::ClearCell(size_t x, size_t y) { data[y * width + x] = -1; }
 
 // Put block on board starts from cell (x, y)
 // return true if ok
@@ -168,7 +170,7 @@ bool Board::SetBlock(const Block &block, size_t x, size_t y)
 
     for (auto &c : block.data)
     {
-        SetCell(x + c.first, y + c.second);
+        SetCell(x + c.first, y + c.second, block.GetIndex());
     }
 
     count_setblock++;
@@ -202,18 +204,18 @@ void Board::ClearBlock(const Block &block, size_t x, size_t y)
 }
 
 // This is the core solution
-bool Board::FindSolution(const Blocks &blocks)
+bool Board::Search(const Block::Blocks &blocks)
 {
-    static size_t block_index = 0;
+    static size_t depth = 0;
 
     // If no block left, solution found!
-    if (block_index == blocks.size())
+    if (depth == blocks.size())
     {
-        block_index--;  // Return to the previous block
+        depth--;  // Return to the previous block
         return true;
     }
 
-    const Block &block = blocks[block_index];
+    const Block &block = blocks[depth];
 
     size_t x_max = width - block.width;
     size_t y_max = height - block.height;
@@ -225,14 +227,11 @@ bool Board::FindSolution(const Blocks &blocks)
             // if there is a place for block, process.
             if (SetBlock(block, x, y))
             {
-                block_index++;  // Try the next block
-                if (FindSolution(blocks))
+                depth++;  // Try the next block
+                if (Search(blocks))
                 {
-                    // If all other blocks are positioned correctly, solution found.
-                    std::cout << "Step: Put block " << block_index << " on [" << x << ", " << y
-                              << "]\n";
-                    block.Print(width, height, x, y, "Block " + std::to_string(block_index));
-                    block_index--;  // Return to the previous block
+                    steps[block.GetIndex()] = std::make_pair(x, y);
+                    depth--;  // Return to the previous block
                     return true;
                 }
 
@@ -243,52 +242,50 @@ bool Board::FindSolution(const Blocks &blocks)
         }
     }
 
-    block_index--;  // Return to the previous block
+    depth--;  // Return to the previous block
     return false;
 }
 
 // Start solving the problem.
-bool Board::Resolve(const Blocks &blocks)
+Board::Solution Board::Solve(const Block::Blocks &blocks)
 {
-    Blocks copy(blocks);
+    Block::Blocks copy(blocks);
 
     // Sort Blocks by block weight
     std::sort(copy.begin(), copy.end(), [](Block &a, Block &b) { return (a.weight > b.weight); });
 
-    size_t i = 0;
-    for (auto &block : copy)
-    {
-        block.Print("Block " + std::to_string(i++));
-    }
-
     // Find solution
-    bool result = FindSolution(copy);
+    bool found = Search(copy);
+    if (!found) steps.clear();
 
-    std::cout << "count_setblock   = " << count_setblock
-              << "\ncount_checkblock = " << count_checkblock
-              << "\ncount_setcell    = " << count_setcell
-              << "\ncount_checkcell  = " << count_checkcell << std::endl;
-
-    return result;
+    return steps;
 }
 
-void Board::Print(const std::string &title) const
+void Board::Print() const
 {
-    std::cout << title << "\n   ";
+    static const std::string pattern("=*-o+v~^");
 
+    std::cout << "   ";
     for (size_t i = 0; i < width; i++)
     {
-        std::cout << std::setw(2) << i;
+        std::cout << std::setw(3) << i;
     }
 
     for (size_t i = 0; i < width * height; i++)
     {
         if (i % width == 0)
         {
-            std::cout << '\n' << std::setw(2) << i / width << ' ';
+            std::cout << '\n' << std::setw(3) << i / width << ' ';
         }
 
-        std::cout << (data[i] ? "[]" : "__");
+        if (data[i] == -1)
+        {
+            std::cout << " . ";
+        }
+        else
+        {
+            std::cout << '[' << pattern[data[i] % pattern.size()] << ']';
+        }
     }
 
     std::cout << '\n' << std::endl;
@@ -298,82 +295,107 @@ int main()
 {
     struct
     {
-        Board  Board;
-        Blocks Blocks;
+        Board         Board;
+        Block::Blocks Blocks;
     } puzzles[] = {
         {
             Board(10, 6),
-            Blocks{
-                Block("*****|* *"),
-                Block(" ***|***|  *"),
-                Block(" **|***| **"),
-                Block(" *|**|*|*"),
-                Block(" *|****| *"),
-                Block("*|***|  **|  *"),
-                Block("*|***| **|  *"),
-                Block(" * **|****"),
-                Block("  **|*****"),
+            Block::Blocks{
+                Block(0, "*****|* *"),
+                Block(1, " ***|***|  *"),
+                Block(2, " **|***| **"),
+                Block(3, " *|**|*|*"),
+                Block(4, " *|****| *"),
+                Block(5, "*|***|  **|  *"),
+                Block(6, "*|***| **|  *"),
+                Block(7, " * **|****"),
+                Block(8, "  **|*****"),
             },
         },
         {
             Board(5, 6),
-            Blocks{
-                Block("**"),
-                Block("***"),
-                Block("****"),
-                Block("*|**| *"),
-                Block("*|*|**"),
-                Block("*|**|*"),
-                Block("**|*"),
-                Block("** *| ***"),
+            Block::Blocks{
+                Block(0, "**"),
+                Block(1, "***"),
+                Block(2, "****"),
+                Block(3, "*|**| *"),
+                Block(4, "*|*|**"),
+                Block(5, "*|**|*"),
+                Block(6, "**|*"),
+                Block(7, "** *| ***"),
             },
         },
         {
             Board(10, 3),
-            Blocks{
-                Block("***|* *"),
-                Block("*|***"),
-                Block(" **|**"),
-                Block("*|*"),
-                Block("****|   *"),
-                Block("**|**"),
-                Block(" *|**"),
-                Block("***"),
+            Block::Blocks{
+                Block(0, "***|* *"),
+                Block(1, "*|***"),
+                Block(2, " **|**"),
+                Block(3, "*|*"),
+                Block(4, "****|   *"),
+                Block(5, "**|**"),
+                Block(6, " *|**"),
+                Block(7, "***"),
             },
         },
         {
             Board(6, 5),
-            Blocks{
-                Block("***|*"),
-                Block("****"),
-                Block(" *|**|*"),
-                Block("*|**"),
-                Block("***"),
-                Block("**|**"),
-                Block("*|*"),
-                Block("  *|* *|***"),
+            Block::Blocks{
+                Block(0, "***|*"),
+                Block(1, "****"),
+                Block(2, " *|**|*"),
+                Block(3, "*|**"),
+                Block(4, "***"),
+                Block(5, "**|**"),
+                Block(6, "*|*"),
+                Block(7, "  *|* *|***"),
             },
         },
         {
             Board(5, 6),
-            Blocks{
-                Block("***"),
-                Block("***| *| *"),
-                Block("**|**"),
-                Block("*|**"),
-                Block(" **| *|**"),
-                Block("*|*"),
-                Block("***|  *"),
-                Block("*|**| *"),
+            Block::Blocks{
+                Block(0, "***"),
+                Block(1, "***| *| *"),
+                Block(2, "**|**"),
+                Block(3, "*|**"),
+                Block(4, " **| *|**"),
+                Block(5, "*|*"),
+                Block(6, "***|  *"),
+                Block(7, "*|**| *"),
             },
         },
     };
 
     auto &puzzle = puzzles[4];
-    if (!puzzle.Board.Resolve(puzzle.Blocks))
+
+    for (auto &block : puzzle.Blocks)
+    {
+        std::cout << "Block " + std::to_string(block.GetIndex()) << '\n';
+        block.Print();
+    }
+
+    auto steps = puzzle.Board.Solve(puzzle.Blocks);
+    if (steps.size() != 0)
+    {
+        std::cout << "Steps\n";
+        for (auto &s : steps)
+        {
+            std::cout << "- Put block " << s.first << " on [" << s.second.first << ", "
+                      << s.second.second << "]\n";
+        }
+
+        std::cout << "\nBoard\n";
+        puzzle.Board.Print();
+    }
+    else
     {
         std::cout << "No solution!" << std::endl;
     }
+
+    std::cout << "count_setblock   = " << count_setblock
+              << "\ncount_checkblock = " << count_checkblock
+              << "\ncount_setcell    = " << count_setcell
+              << "\ncount_checkcell  = " << count_checkcell << std::endl;
 
     return 0;
 }
